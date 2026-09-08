38c5e1f8972812d2769d3b8c6421dbd3d0557436
sarina-loli <sarina123getu@gmail.com>
2026-09-08 06:32:24 -0700

    Fix Django settings module

--- smartapp/asgi.py
+++ smartapp/asgi.py
@@ -6,6 +6,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartapp.settings')
 
 application = get_asgi_application()
